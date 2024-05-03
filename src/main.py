038350605cff3e8ec3c98a1065596321fe781aff
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-05-03 13:46:29 -0300

    Unit Tests

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+def main():
+	print('Hello world')
+	
+	test_obj = TextNode('This is a text node', 'bold', 'https://www.boot.dev')
+	print(test_obj)	
+
+if __name__ == "__main__":
+    main()
